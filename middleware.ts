9038bec0befb6ddf163fb5c1dc7661c9c1d1f217
waman-pawar-pst <97658515+waman-pawar-pst@users.noreply.github.com>
2023-12-17 00:36:42 +0530

    midified middlewere

--- middleware.ts
+++ middleware.ts
@@ -8,10 +8,10 @@ export default authMiddleware({
       return redirectToSignIn({ returnBackUrl: req.url });
     }
     // Redirect logged in users to organization selection page if they are not active in an organization
-    if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/org-selection"){
-      const orgSelection = new URL('/org-selection', req.url)
-      return NextResponse.redirect(orgSelection)
-    }
+    // if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/org-selection"){
+    //   const orgSelection = new URL('/org-selection', req.url)
+    //   return NextResponse.redirect(orgSelection)
+    // }
     // If the user is logged in and trying to access a protected route, allow them to access route
     if (auth.userId && !auth.isPublicRoute) {
       return NextResponse.next()
