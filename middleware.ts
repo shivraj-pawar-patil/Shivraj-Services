14c99667fcdc6ca43c76e1cb66ba32ab4332fcef
waman-pawar-pst <97658515+waman-pawar-pst@users.noreply.github.com>
2023-12-17 00:53:49 +0530

    Allowed all user to see the pages

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server'; 
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
- 
+import { NextResponse } from "next/server";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+
 export default authMiddleware({
   afterAuth(auth, req, evt) {
     // Handle users who aren't authenticated
@@ -8,15 +8,19 @@ export default authMiddleware({
       return redirectToSignIn({ returnBackUrl: req.url });
     }
     // Redirect logged in users to organization selection page if they are not active in an organization
-    // if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/org-selection"){
-    //   const orgSelection = new URL('/org-selection', req.url)
-    //   return NextResponse.redirect(orgSelection)
-    // }
+    if (
+      auth.userId &&
+      !auth.orgId &&
+      req.nextUrl.pathname !== "/home"
+    ) {
+      const orgSelection = new URL("/home", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
     // If the user is logged in and trying to access a protected route, allow them to access route
     if (auth.userId && !auth.isPublicRoute) {
-      return NextResponse.next()
+      return NextResponse.next();
     }
     // Allow users visiting public routes to access them
     return NextResponse.next();
-  }
+  },
 });
