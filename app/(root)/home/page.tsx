560ede729ce43e0b0d3fb79d0876f8948871621b
waman-pawar-pst <97658515+waman-pawar-pst@users.noreply.github.com>
2023-12-16 23:47:06 +0530

    Remove unwanted code

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,17 +1,9 @@
-import prisma from "@/lib/prisma";
 import React from "react";
 
 async function page() {
-  const data = await prisma.test.findMany({
-    select: {
-      title: true,
-      id: true,
-    },
-  });
   return (
     <div>
       Home
-      {JSON.stringify(data)}
     </div>
   );
 }
