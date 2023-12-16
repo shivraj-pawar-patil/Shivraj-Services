2578253b9b0e85335ea1a4f013fceab96f8f5972
waman-pawar-pst <97658515+waman-pawar-pst@users.noreply.github.com>
2023-12-16 16:52:43 +0530

    added prisma config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
