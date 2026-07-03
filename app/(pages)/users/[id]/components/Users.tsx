616f2d2205dcfed05fd3016eb0c76094ba5063ab
shivraj-pawar-patil <shivraj.patil1209@gmail.com>
2026-07-03 15:20:16 +0530

    Added change for serial number

--- app/(pages)/users/[id]/components/Users.tsx
+++ app/(pages)/users/[id]/components/Users.tsx
@@ -80,8 +80,8 @@ export type User = {
 };
 
 export default function DataTableDemo({ users }: { users: User[] }) {
-  const { organization } = useOrganization();
 
+  console.log("Users data received in DataTableDemo:", users); // Debugging line
   // State for filters
   const [type, setType] = React.useState("");
   const [selectedDate, setSelectedDate] = React.useState<Date | undefined>(
